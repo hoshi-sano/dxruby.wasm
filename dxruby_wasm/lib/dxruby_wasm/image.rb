4467c59248baef1d7516c36511b1b3b6ee0517ff
hoshi-sano <hoshi.sanou@gmail.com>
2025-08-25 01:06:37 +0900

    Add Image.load, Image#load

--- dxruby_wasm/lib/dxruby_wasm/image.rb
+++ dxruby_wasm/lib/dxruby_wasm/image.rb
@@ -4,12 +4,33 @@ module DXRubyWasm
   class Image
     attr_reader :canvas, :width, :height
 
+    def self.load(path_or_url)
+      new(1, 1).load(path_or_url)
+    end
+
     def initialize(width, height, color = C_DEFAULT, canvas: nil)
       @width, @height = width, height
       @canvas = canvas || JS.global[:document].createElement("canvas")
       @ctx = @canvas.getContext("2d")
       resize(@width, @height)
       box_fill(0, 0, @width, @height, color)
+    end
+
+    def load(path_or_url)
+      img = JS.global[:Image].new(1, 1)
+      # TODO: load from wasi file system
+      img[:src] = path_or_url
+
+      img.decode.then {
+        img[:width] = img[:naturalWidth]
+        img[:height] = img[:naturalHeight]
+        # TODO: update img.style.width, img.style.height here ?
+        resize(img[:naturalWidth], img[:naturalHeight])
+        @ctx.drawImage(img, 0, 0)
+      }.catch { |e|
+        raise DXRubyWasm::Error, "Failed to load image: #{path_or_url}"
+      }
+      self
     end
 
     def draw(x, y, image)
