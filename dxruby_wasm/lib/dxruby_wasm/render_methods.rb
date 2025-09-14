4de35901ebec2fffdb1300f0a32cfbbd17504e1d
hoshi-sano <hoshi.sanou@gmail.com>
2025-09-15 01:43:53 +0900

    Add shape and line drawing methods

--- dxruby_wasm/lib/dxruby_wasm/render_methods.rb
+++ dxruby_wasm/lib/dxruby_wasm/render_methods.rb
@@ -24,6 +24,39 @@ module DXRubyWasm
       enqueue_draw(z, :font, x, y, text, font, options)
     end
 
+    def draw_pixel(x, y, color, z = 0)
+      # TODO:
+      raise
+    end
+
+    def draw_line(x1, y1, x2, y2, color, z = 0)
+      enqueue_draw(z, :line, x1, y1, x2, y2, color)
+    end
+
+    def draw_box(x1, y1, x2, y2, color, z = 0)
+      enqueue_draw(z, :box, x1, y1, x2, y2, color)
+    end
+
+    def draw_box_fill(x1, y1, x2, y2, color, z = 0)
+      enqueue_draw(z, :box_fill, x1, y1, x2, y2, color)
+    end
+
+    def draw_circle(x, y, r, color, z = 0)
+      enqueue_draw(z, :circle, x, y, r, color)
+    end
+
+    def draw_circle_fill(x, y, r, color, z = 0)
+      enqueue_draw(z, :circle_fill, x, y, r, color)
+    end
+
+    def draw_triangle(x1, y1, x2, y2, x3, y3, color, z = 0)
+      enqueue_draw(z, :triangle, x1, y1, x2, y2, x3, y3, color)
+    end
+
+    def draw_triangle_fill(x1, y1, x2, y2, x3, y3, color, z = 0)
+      enqueue_draw(z, :triangle_fill, x1, y1, x2, y2, x3, y3, color)
+    end
+
     def enqueue_draw(z, *args)
       @draw_queue.push([z, @draw_queue.length, *args])
     end
@@ -41,6 +74,14 @@ module DXRubyWasm
         when :image then @image.draw(*args)
         when :draw_ex then @image.draw_ex(*args)
         when :font then @image.draw_font_ex(*args)
+        when :pixel then raise # TODO:
+        when :line then @image.line(*args)
+        when :box then @image.box(*args)
+        when :box_fill then @image.box_fill(*args)
+        when :circle then @image.circle(*args)
+        when :circle_fill then @image.circle_fill(*args)
+        when :triangle then @image.triangle(*args)
+        when :triangle_fill then @image.triangle_fill(*args)
         end
       }
     end
